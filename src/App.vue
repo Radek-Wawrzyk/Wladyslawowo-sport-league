5a2c82a1c31f55a731ba8adb23d680d313c8d120
Radek <radek511@op.pl>
2018-08-04 20:04:14 +0200

    Remove - App.vue unnecessary content
    Also change content in index.html file

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,13 @@
 <template>
   <div id="app">
-    <h1 class="title is-1 has-text-centered">Liga gminy Władysławowo</h1>
-    <router-view/>
-    <section class="section">
-      <div id="test" class="container">
-        <input class="input" v-model="name">
-        <button class="button is-warning" @click="test" >Add to firebase</button>
-        <ul class="list">
-          <li v-for="(item) in tests" :key="item.index">{{item.test}}</li>
-        </ul>
-      </div>
-    </section>
-    
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 
-import { namesRef } from "./firebase/";
-
 export default {
-  name: "App",
-  data() {
-    return {
-      name: ""
-    }
-  },
-  firebase: {
-    tests: namesRef
-  },
-  methods: {
-    test() {
-      namesRef.push({test: this.name});
-    }
-  }
+  name: "App"
 }	
 
 </script>
