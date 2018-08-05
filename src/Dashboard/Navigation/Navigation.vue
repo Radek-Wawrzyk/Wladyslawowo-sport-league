dd7aff432d5848a2129ff9b83cd4bf60169a9607
Radek <radek511@op.pl>
2018-08-05 03:31:58 +0200

    Add - Animation for Navigation dropdown

--- src/Dashboard/Navigation/Navigation.vue
+++ src/Dashboard/Navigation/Navigation.vue
@@ -29,7 +29,7 @@
             <path d="m286.94 69.377c-3.614-3.617-7.898-5.424-12.848-5.424h-255.81c-4.952 0-9.233 1.807-12.85 5.424-3.617 3.621-5.424 7.902-5.424 12.851 0 4.948 1.807 9.229 5.424 12.847l127.91 127.91c3.621 3.617 7.902 5.428 12.85 5.428s9.233-1.811 12.847-5.428l127.91-127.91c3.613-3.617 5.427-7.898 5.427-12.847 0-4.948-1.814-9.229-5.427-12.85z"/>
           </svg>
         </button>
-        <transition name="fade" tag="div">
+        <transition name="fade-normal" tag="div">
           <div class="navigation-dropdown" :class="{'is-active': dropDown}" v-if="dropDown">
             <ul class="navigation-dropdown-content">
               <li>Profil użytkownika</li>
