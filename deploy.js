7f134ea74863f3f43f011f45aca623e7b3959994
MatisiekPL <matisiek11@gmail.com>
2018-08-06 02:58:48 +0800

    added - deploy script

--- deploy.js
+++ deploy.js
@@ -1,1 +1,11 @@
-const 
+const fs = require( 'vinyl-fs' );
+const ftp = require( 'vinyl-ftp' );
+const  conn = ftp.create( {
+        host: process.env.FTP_HOST,
+        user: process.env.FTP_USER,
+        password: process.env.FTP_PASS,
+        parallel: 10,
+        log: console.log
+    } );
+fs.src( [ './dist/**' ], { buffer: false } )
+    .pipe( conn.dest( process.env.FTP_DIST ) );
