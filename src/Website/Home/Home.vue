8754a6b3a5a7ec69f7f3fd1001a5dff5e8055015
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2018-08-06 12:57:16 +0200

    Add - Basic skeleton of Home.vue with assigned columns and a few dump-data

--- src/Website/Home/Home.vue
+++ src/Website/Home/Home.vue
@@ -1,13 +1,68 @@
 <template>
-    <main>Home</main>
-</template>
-<style lang="scss" scoped>
+	<main class="home-container">
+		<header class="home-baner">
+			<div class="columns">
+				<div class="column">
+					<div class="baner-frame">
+						<img class="baner-image" src="http://placehold.it/1380x540"/>
+					</div>
+					<div class="baner-content">
+							<h4>Piraci Władysławowo znów na prowadzeniu!</h4>
+							<p>Piraci Władysławowo wyprzedzili dotychczasowych 
+									mistrzów aż o 20 punktów w klasyfikacji generalnej</p>
+					</div>
+				</div>
+			</div>
+		</header>
+		<div class="columns">
+			<div class="column is-one-fifth"></div>
+			<div class="column is-three-fifths">
+				<div class="home-main">
+					<div class="home-events">
+						<img src="http://placehold.it/950x550"/>
+						<div class="events-contents">
+							<div class="columns contents-columns">
+								<div class="column">
+									<div class="events-topbar">
+										Sprawdź nadchodzące wydarzenia!
+									</div>
+									<div class="events-bottombar">
+										Najbliższy mecz ligowy już pod koniec lipca!
+									</div>
+								</div>
+								<div class="column">
+								<aside class="events-sidebar">
+									<div class="sidebar-item">
+											
+									</div>
+									<div class="sidebar-item">
 
-</style>
+									</div>
+									<div class="sidebar-item">
+
+									</div>
+									<div class="sidebar-item">
+
+									</div>
+								</aside>
+							</div>
+
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="column is-two-fifths">
+				<aside class="home-top">
+
+				</aside>
+			</div>
+		</div>
+	</main>
+</template>
+<style lang="scss" src="./Home.scss" scoped></style>
 <script>
 export default {
-    name: "Home"
-}
+  name: "Home"
+};
 </script>
-
-
