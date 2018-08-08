970c9c741e5fa92ba99eaa59e09f06bc9733af0e
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2018-08-08 13:54:48 +0200

    Add - Contact form to the Project

--- src/Website/Contact/Contact.vue
+++ src/Website/Contact/Contact.vue
@@ -1,7 +1,33 @@
 <template>
-    <main>Contact</main>
+    <main class="contact-container">
+      <div class="contact-box">
+				<div class="contact-header">
+					<h4 class="header-text">Zadzwoń do nas bądź skorzystaj z naszego formularza!</h4>
+				</div>
+				<div class="contact-data">
+					<img class="contact-img" src="@/Assets/logo.jpg"/>
+					<table class="contact-text">
+						<tr>Dane Kontaktowe:</tr>
+						<tr>Imię i Nazwisko: Marcin X</tr>
+						<tr>Telefon: 666 666 666</tr>
+						<tr>E-mail: xd@gmail.com</tr>
+					</table>
+				</div>
+				<div class="contact-form">
+					<h4 class="form-header">
+						Formularz kontaktowy
+					</h4>
+					<form class="form">
+							<input class="input-name" type="text" placeholder="Imię"/>
+							<input class="input-email" type="text" placeholder="E-mail"/>
+							<input class="input-title" type="text" placeholder="Temat wiadomości"/>
+							<textarea class="input-msg" placeholder="Treść wiadomości"></textarea>
+					</form>
+				</div>
+      </div>
+    </main>
 </template>
-<style lang="scss" scoped>
+<style lang="scss" src="./Contact.scss" scoped>
 
 </style>
 <script>
