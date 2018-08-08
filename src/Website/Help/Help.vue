0fe9e46a98856969f2d0a10d188651247df10f91
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2018-08-08 13:03:39 +0200

    Implement Help component and fill it's contents with dummy data

--- src/Website/Help/Help.vue
+++ src/Website/Help/Help.vue
@@ -1,7 +1,26 @@
 <template>
-    <main>Help</main>
+    <main class="help-container">
+      <div class="help-regulations">
+        <div class="regulations-title">
+          <h4>Regulamin</h4>
+        </div>
+        <div class="regulations-content">
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+        </div>
+      </div>
+      <div class="help-download">
+        <div class="download-title">
+          <h4>Pobierz Regulaminy</h4>
+        </div>
+        <div class="download-buttons">
+          <button class="main-regulations">Pobierz regulamin główny w .PDF</button>
+          <button class="scoring-regulations">Pobierz regulamin punktowania w .PDF</button>
+        </div>
+      </div>
+    </main>
 </template>
-<style lang="scss" scoped>
+<style lang="scss" src="./Help.scss" scoped>
 </style>
 <script>
 export default {
