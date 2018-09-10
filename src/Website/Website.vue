59b5b9a0f37ad9f4f10b8ec4fba086b5e8ebecc6
Radek <radek511@op.pl>
2018-09-10 19:15:40 +0200

    Refactor - Website main component

--- src/Website/Website.vue
+++ src/Website/Website.vue
@@ -1,24 +1,22 @@
 <template>
-    <div>
-        <Navigation/>
-        <router-view>
-        </router-view>
-        <Footer/>
-    </div>
+  <div class="website">
+    <Navigation></Navigation>
+    <router-view></router-view>
+    <Footer></Footer>
+  </div>
 </template>
 
 <script>
+
 import Navigation from "./Navigation/Navigation.vue"
 import Footer from "./Footer/Footer.vue"
 
 export default {
-    name: "Website",
-    components: 
-    {
-        Navigation,
-        Footer
-    }
+  name: "Website",
+  components: {
+    Navigation,
+    Footer
+  }
 }
-</script>
 
-<style lang="scss" scoped></style>
+</script>
