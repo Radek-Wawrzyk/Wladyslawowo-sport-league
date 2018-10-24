d5d19bcf7101c315471e6d5dd973109e9d2f5e64
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2018-10-24 18:55:10 +0200

    Create getter that returns every item from Players, Settlements, News and Events that matches given string pattern

--- src/Store/Modules/Shared.js
+++ src/Store/Modules/Shared.js
@@ -1,13 +1,37 @@
 import router from '@/Router/index';
+import players from './Players'
+import events from './Events'
+import news from './News'
+import settlements from './Settlements'
+
 
 export default {
   state: {
     menuStatus: false,
   },
+  players,
+  events,
+  news,
+  settlements,
   getters: {
     menu: state => {
       return state.menuStatus;
     },
+    search: state => phrase =>
+    {
+      phrase = phrase.toLowerCase();
+
+      let playerMatches = players.getters.players(players.state).filter(p => p.name.toLowerCase().includes(phrase));
+      let eventMatches = events.getters.events(events.state).filter(e => e.name.toLowerCase().includes(phrase));
+      let newsMatches = news.getters.news(news.state).filter(n => n.name.toLowerCase().includes(phrase));
+      let settlementsMatches = settlements.getters.settlements(settlements.state).filter(s => s.name.toLowerCase().includes(phrase));
+      return{
+        players: playerMatches,
+        events: eventMatches,
+        news: newsMatches,
+        settlements: settlementsMatches
+      };
+    }
   },
   mutations: {
     closeModal: () => {
