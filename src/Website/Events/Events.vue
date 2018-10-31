9ee650863c417419350b4cd6550c82ac1f7a31d4
Radek <radek511@op.pl>
2018-10-31 01:14:04 +0000

    Edit - Events tyles

--- src/Website/Events/Events.vue
+++ src/Website/Events/Events.vue
@@ -1,170 +1,42 @@
 <template>
-	<main class="website-container events-container">
-		<div class="events-baner">
-			<img class="baner-img" src="@/Assets/Home/events.jpg"/>
-			<div class="baner-text">
-				Sprawdź kiedy odbędzie się najbliższa impreza sportowa!
-			</div>
-		</div>
-		<div class="events-table">
-			<div class="table-heading">
-				<h4>Najbliższe wydarzenia sportowe</h4>
-					<div class="list-buttons">
-					<button @click="moveLeft" class="arrow-left">
-						<svg aria-hidden="true" data-prefix="fas" data-icon="chevron-left" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" class="svg-inline--fa fa-chevron-left fa-w-10 fa-3x"><path fill="currentColor" d="M34.52 239.03L228.87 44.69c9.37-9.37 24.57-9.37 33.94 0l22.67 22.67c9.36 9.36 9.37 24.52.04 33.9L131.49 256l154.02 154.75c9.34 9.38 9.32 24.54-.04 33.9l-22.67 22.67c-9.37 9.37-24.57 9.37-33.94 0L34.52 272.97c-9.37-9.37-9.37-24.57 0-33.94z"></path></svg>
-					</button>
-					<button @click="moveRight" class="arrow-right">
-						<svg aria-hidden="true" data-prefix="fas" data-icon="chevron-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" class="svg-inline--fa fa-chevron-right fa-w-10 fa-2x"><path fill="currentColor" d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"></path></svg>
-					</button>
-				</div>
-			</div>
-			<div class="table-list">
-				<div ref="tableFrame" v-bind:style="{left : frameLeft}" class="table-frame">
-					<div class="list-item">
-						<div class="item-heading">
-							Baseball - Turniej Juniorów
-						</div>
-						<img src="@/Assets/Home/info.jpg" class="item-background"/>
-						<div class="item-footer">
-							12 Lipiec 2018 - 18:00 
-						</div>
-					</div>
-					<div class="list-item">
-						<div class="item-heading">
-							Baseball - Turniej Juniorów
-						</div>
-						<img src="@/Assets/Home/info.jpg" class="item-background"/>
-						<div class="item-footer">
-							12 Lipiec 2018 - 18:00 
-						</div>
-					</div>
-					<div class="list-item">
-						<div class="item-heading">
-							Baseball - Turniej Juniorów
-						</div>
-						<img src="@/Assets/Home/info.jpg" class="item-background"/>
-						<div class="item-footer">
-							12 Lipiec 2018 - 18:00 
-						</div>
-					</div>
-					<div class="list-item">
-						<div class="item-heading">
-							Baseball - Turniej Juniorów
-						</div>
-						<img src="@/Assets/Home/info.jpg" class="item-background"/>
-						<div class="item-footer">
-							12 Lipiec 2018 - 18:00 
-						</div>
-					</div>
-					<div class="list-item">
-						<div class="item-heading">
-							Baseball - Turniej Juniorów
-						</div>
-						<img src="@/Assets/Home/info.jpg" class="item-background"/>
-						<div class="item-footer">
-							12 Lipiec 2018 - 18:00 
-						</div>
-					</div>
-					<div class="list-item">
-						<div class="item-heading">
-							Baseball - Turniej Juniorów
-						</div>
-						<img src="@/Assets/Home/info.jpg" class="item-background"/>
-						<div class="item-footer">
-							12 Lipiec 2018 - 18:00 
-						</div>
-					</div>
-					<div class="list-item">
-						<div class="item-heading">
-							Baseball - Turniej Juniorów
-						</div>
-						<img src="@/Assets/Home/info.jpg" class="item-background"/>
-						<div class="item-footer">
-							12 Lipiec 2018 - 18:00 
-						</div>
-					</div>
-					<div class="list-item">
-						<div class="item-heading">
-							Baseball - Turniej Juniorów
-						</div>
-						<img src="@/Assets/Home/info.jpg" class="item-background"/>
-						<div class="item-footer">
-							12 Lipiec 2018 - 18:00 
-						</div>
-					</div>
-					<div class="list-item">
-						<div class="item-heading">
-							Baseball - Turniej Juniorów
-						</div>
-						<img src="@/Assets/Home/info.jpg" class="item-background"/>
-						<div class="item-footer">
-							12 Lipiec 2018 - 18:00 
-						</div>
-					</div>
-					<div class="list-item">
-						<div class="item-heading">
-							Baseball - Turniej Juniorów
-						</div>
-						<img src="@/Assets/Home/info.jpg" class="item-background"/>
-						<div class="item-footer">
-							12 Lipiec 2018 - 18:00 
-						</div>
-					</div>
-					<div class="list-item">
-						<div class="item-heading">
-							Baseball - Turniej Juniorów
-						</div>
-						<img src="@/Assets/Home/info.jpg" class="item-background"/>
-						<div class="item-footer">
-							12 Lipiec 2018 - 18:00 
-						</div>
-					</div>
-				</div>
-			</div>
+	<main class="website-container">
+		<header class="website-header">
+			<h1 class="title is-3">Imprezy</h1>
+		</header>
+		<div class="table-responsive">
+			<table class="table-panel">
+				<thead>
+				<tr>
+					<th>Lp</th>
+					<th>Nazwa imprezy</th>
+					<th>Data</th>
+					<th>Ilość zawodników</th>
+				</tr>
+				</thead>
+				<tbody class="table-body">
+				<tr v-for="(event, index) in events" :key="index" @click="$router.push(`/events/${event.id}`)" :aria-label="event.name" :title="event.name">
+					<th>{{index +1}}</th>
+					<th>{{event.name}}</th>
+					<th>{{event.date}}</th>
+					<th>{{event.players.length}}</th>
+				</tr>
+				</tbody>
+			</table>
 		</div>
 	</main>
 </template>
-<style src="./Events.scss" lang="scss" scoped>
 
-</style>
 <script>
+
 export default {
-    name: "Events",
-		data: function()
-		{
-			return{
-				left: 0
-			}
-		},
-		computed:
-		{
-			frameLeft: function()
-			{
-				return this.left + 'px';
-			}
-		},
-		methods:
-		{
-			moveRight()
-			{
-				console.log(this.left);
-				console.log(-this.$refs.tableFrame.childNodes[0].clientWidth * this.$refs.tableFrame.childNodes.length);
-				if(this.left >= ((-this.$refs.tableFrame.childNodes[0].clientWidth + 10) * this.$refs.tableFrame.childNodes.length /2) + this.$refs.tableFrame.clientWidth)
-				{
-					this.left -= this.$refs.tableFrame.childNodes[0].clientWidth;
-				}
-				/*console.log(this.$refs.tableFrame.clientWidth); //Ramka
-				console.log(this.$refs.tableFrame.childNodes[0].clientWidth); //Szerokość
-				console.log(this.$refs.tableFrame.childNodes[0].clientHeight); //Wysokość
-				console.log(this.$refs.tableFrame.childNodes.length);*/
-			},
-			moveLeft()
-			{
-				if(this.left < 0)
-					this.left += this.$refs.tableFrame.childNodes[0].clientWidth;
-			}
+	name: "Events",
+	computed: {
+	  events() {
+	    return this.$store.getters.events;
 		}
+	}
 }
+
 </script>
 
 
